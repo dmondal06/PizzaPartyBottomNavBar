55b2d69cfffa4d0f67be9ab21cdeede05979f708
dmondal06 <deepa.mondal321@gmail.com>
2024-10-22 14:33:00 -0400

    completed todo 1

--- app/src/main/java/edu/farmingdale/pizzapartybottomnavbar/ui/SplashScreen.kt
+++ app/src/main/java/edu/farmingdale/pizzapartybottomnavbar/ui/SplashScreen.kt
@@ -1,15 +1,12 @@
-package edu.farmingdale.pizzapartybottomnavbar
+package edu.farmingdale.pizzapartybottomnavbar.ui
 
 import android.view.animation.OvershootInterpolator
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
@@ -17,10 +14,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavHostController
+import edu.farmingdale.pizzapartybottomnavbar.BottomNavigationItems
+import edu.farmingdale.pizzapartybottomnavbar.R
 import kotlinx.coroutines.delay
 
 
-// ToDo 1: The splash screen should have the image displayed in the center of the screen.
+// ToDo 1: Done The splash screen should have the image displayed in the center of the screen.
 
 @Composable
 fun SplashScreen(navController: NavHostController) {
@@ -39,9 +38,16 @@ fun SplashScreen(navController: NavHostController) {
         navController.navigate(BottomNavigationItems.PizzaScreen.route)
     }
 
-    Column (modifier = Modifier
-        .wrapContentSize(Alignment.Center)){
-        Image(painter = painterResource(id = R.drawable.fsclogo), contentDescription ="" )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.fsclogo),
+            contentDescription = "",
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
     }
 }
 
